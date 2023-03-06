17009e4ca3a96324c1fbfaf28a8936f0412258b8
victordelcarpio <victor.delcarpiogomez@yale.edu>
2023-03-06 16:27:47 -0500

    correctly gets length of all of the across clues

--- parse.py
+++ parse.py
@@ -1,21 +1,11 @@
-# from bs4 import BeautifulSoup
-# import urllib.request
-# from selenium import webdriver
-
-# url = "https://www.downforacross.com/beta/game/2965282-thrun"
-# html = urllib.request.urlopen(url).read()
-# soup = BeautifulSoup(html, features="html.parser")
-# print(soup)
-# tags = soup.findAll("td", class_="grid--cell")
-# print(tags)
-
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+import math
 
-url = "https://www.downforacross.com/beta/game/2965282-thrun"
+url = "https://www.downforacross.com/beta/game/2965282-thrun" # beta url for testing
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
@@ -27,9 +17,44 @@ driver.quit()
 os.system("cls")
 
 soup = BeautifulSoup(page, 'html.parser')
-tags = soup.findAll("td", class_="grid--cell")    
-
+tags = soup.findAll("td", class_="grid--cell")  
 clues = soup.findAll("div", class_="clues--list--scroll--clue")
-print(tags[0])
-print(type(tags[0]))
-print(clues[0])
+
+dimensions: int = int(math.sqrt(len(tags)))
+across: dict = {
+    # index of clue : length of solution
+}
+down: dict = {
+    # index of clue : length of solution
+}
+
+
+for k in range(dimensions):
+    current_across = None
+    length_across = 0
+
+    for i in range(dimensions):
+        index: int = (dimensions * k) + i
+        content: str = str(tags[index].contents[0])
+        tag = BeautifulSoup(content, 'html.parser')
+        divs = tag.find_all('div')
+
+        # Reset when we encounter a black square
+        if len(divs) == 2:
+            across[current_across] = length_across
+
+            current_across = None
+            length_across = 0
+        
+        else:
+            number: list = divs[2].contents
+            if len(number) != 0:
+                if current_across is None:
+                    current_across = number[0]
+            
+            length_across += 1
+
+    if current_across != None:
+        across[current_across] = length_across
+
+print(across)
