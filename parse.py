8b53f0185a266ee610a4c491dcde4c896a63a7cc
victordelcarpio <victor.delcarpiogomez@yale.edu>
2023-03-06 17:58:41 -0500

    gets all the across clues with the hint number

--- parse.py
+++ parse.py
@@ -5,56 +5,76 @@ from selenium.webdriver.chrome.options import Options
 import os
 import math
 
-url = "https://www.downforacross.com/beta/game/2965282-thrun" # beta url for testing
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=options)
-driver.get(url)
-time.sleep(3)
-page = driver.page_source
-driver.quit()
-os.system("cls")
+def scrape_page(url: str) -> BeautifulSoup:
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(url)
+    time.sleep(3)
+    page = driver.page_source
+    driver.quit()
+    os.system("cls")
 
-soup = BeautifulSoup(page, 'html.parser')
-tags = soup.findAll("td", class_="grid--cell")  
-clues = soup.findAll("div", class_="clues--list--scroll--clue")
+    return BeautifulSoup(page, 'html.parser')
 
-dimensions: int = int(math.sqrt(len(tags)))
-across: dict = {
-    # index of clue : length of solution
-}
-down: dict = {
-    # index of clue : length of solution
-}
+def extract_across(soup: BeautifulSoup) -> dict:
+    tags = soup.findAll("td", class_="grid--cell")
+    dimensions: int = int(math.sqrt(len(tags)))
+    across: dict = {}
 
+    for k in range(dimensions):
+        current_across = None
+        length_across = 0
 
-for k in range(dimensions):
-    current_across = None
-    length_across = 0
+        for i in range(dimensions):
+            index: int = (dimensions * k) + i
+            content: str = str(tags[index].contents[0])
+            tag = BeautifulSoup(content, 'html.parser')
+            divs = tag.find_all('div')
 
-    for i in range(dimensions):
-        index: int = (dimensions * k) + i
-        content: str = str(tags[index].contents[0])
-        tag = BeautifulSoup(content, 'html.parser')
-        divs = tag.find_all('div')
+            # Reset when we encounter a black square
+            if len(divs) == 2:
+                across[current_across] = length_across
 
-        # Reset when we encounter a black square
-        if len(divs) == 2:
+                current_across = None
+                length_across = 0
+            
+            else:
+                number: list = divs[2].contents
+                if len(number) != 0:
+                    if current_across is None:
+                        current_across = number[0]
+                
+                length_across += 1
+
+        if current_across != None:
             across[current_across] = length_across
 
-            current_across = None
-            length_across = 0
-        
-        else:
-            number: list = divs[2].contents
-            if len(number) != 0:
-                if current_across is None:
-                    current_across = number[0]
-            
-            length_across += 1
+    return {k: v for k, v in across.items() if k is not None} # Filter for None values
 
-    if current_across != None:
-        across[current_across] = length_across
+def extract_across_hints(soup: BeautifulSoup) -> dict:
+    across_list = soup.findAll("div", class_="clues--list")
+    across_hints = BeautifulSoup(str(across_list[0]), 'html.parser')
+    clues = across_hints.findAll("div", class_="clues--list--scroll--clue")
 
-print(across)
+    across_clues = {}
+
+    for clue in clues:
+        clue_ = BeautifulSoup(str(clue), 'html.parser')
+        divs = clue_.find_all('div')
+        number = divs[1].text
+        hint = divs[2].text
+        across_clues[number] = hint
+    
+    return across_clues
+
+
+# Driver code
+soup = scrape_page("https://www.downforacross.com/beta/game/2965282-thrun")
+across = extract_across(soup)
+across_hints = extract_across_hints(soup)
+
+for k, v in across.items():
+    if k in across_hints:
+        print(f"{k}: {v}-letter word for the clue: {across_hints[k]}")
